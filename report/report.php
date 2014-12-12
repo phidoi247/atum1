49ac065fb6b840ea040f274ff9d961bead89a38f
phidoi247 <phidoi247@gmail.com>
2014-12-12 15:09:04 +0000

    biểu đồ báo cáo doanh số sản phẩm theo ngày đã xong, giờ cho thêm tí mắm tôm nữa vào là đẹp :v

--- report/report.php
+++ report/report.php
@@ -18,7 +18,7 @@
 			//khai báo biến
 			$tungay = date('Y-m-d',strtotime($_POST['tuNgay']));
 			$denngay = date('Y-m-d',strtotime($_POST['denNgay']));
-			$ten = $_POST['tenBieuDo'];
+			$tenbieudo = $_POST['tenBieuDo'];
 			$subtitle = $_POST['subtitle'];
 			$suffix = $_POST['suffix'];
 			$masp = explode(',',$_POST['maSanPham']);
@@ -29,7 +29,7 @@
 			$qname .= " FROM tblhoadon JOIN tblbanhang ";
 			$qname .= " ON tblhoadon.ten_hoadon = tblbanhang.ten_hoadon	JOIN tblsanpham ";
 			$qname .= " ON tblsanpham.sanpham_id = tblbanhang.sanpham_id ";
-			$qname .= "	WHERE tblhoadon.thoi_gian BETWEEN "."'" .$tungay ."'"." AND "."'".$denngay ."'"." AND tblsanpham.sanpham_id IN ( ";
+			$qname .= "	WHERE tblhoadon.ngay BETWEEN "."'" .$tungay ."'"." AND "."'".$denngay ."'"." AND tblsanpham.sanpham_id IN ( ";
 			for($i=0;$i<count($masp);$i++){//bo dau phay o cuoi cung
 				if($i<count($masp)-1){
 					$qname .= "'"."$masp[$i]"."'".",";
@@ -42,8 +42,8 @@
 			$qname .= " ORDER BY tblsanpham.sanpham_id ASC; ";
 			$r = mysqli_query($dbc, $qname) or die ("Query $qname <br /> mysql error: ".mysqli_errno($dbc));
 				
-			while($ten = mysqli_fetch_array($r, MYSQLI_ASSOC)){
-				$name[] = $ten['ten_sanpham'];
+			while($tensp = mysqli_fetch_array($r, MYSQLI_ASSOC)){
+				$name[] = $tensp['ten_sanpham'];
 			};//kết thúc
 			
 			echo "<pre>";
@@ -64,23 +64,21 @@
 		
 		//lấy data
 		for($i=0;$i<count($masp);$i++){
-			$qdata[$i] = " SELECT tblhoadon.thoi_gian,sum(tblbanhang.soluong)as soluong ";
+			$qdata[$i] = " SELECT tblhoadon.ngay,sum(tblbanhang.soluong)as soluong ";
 			$qdata[$i] .= " FROM tblhoadon JOIN tblbanhang ";
 			$qdata[$i] .= " ON tblhoadon.ten_hoadon = tblbanhang.ten_hoadon ";
 			$qdata[$i] .= " JOIN tblsanpham ON tblsanpham.sanpham_id = tblbanhang.sanpham_id ";
-			$qdata[$i] .= "	WHERE tblsanpham.sanpham_id IN (" ."'".$masp[$i]."'".")";
-			$qdata[$i] .= " GROUP BY tblhoadon.thoi_gian ";
+			$qdata[$i] .= "	WHERE tblhoadon.ngay BETWEEN "."'" .$tungay ."'"." AND "."'".$denngay ."'"." AND tblsanpham.sanpham_id IN (" ."'".$masp[$i]."'".")";
+			$qdata[$i] .= " GROUP BY tblhoadon.ngay ";
+			$qdata[$i] .= " ORDER BY tblsanpham.sanpham_id ASC; ";
 			$r = mysqli_query($dbc, $qdata[$i]);
 			
 			while($dulieu = mysqli_fetch_array($r, MYSQLI_ASSOC)){
-				$thoigian[$i][] = $dulieu['thoi_gian'];
+				$thoigian[$i][] = $dulieu['ngay'];
 				$data[$i][] = $dulieu['soluong'];  
 			};			
 		};
-		for($i=0;$i<count($masp);$i++){
-		echo "<pre>";
-		print_r($qdata);
-		echo "<pre>";};
+		
 		//ép kiểu
 		for($i=0;$i<count($masp);$i++){
 			for($j=0;$j<count($thoigian[$i]);$j++){
@@ -88,32 +86,35 @@
 			};
 		};
 		
-		/*for($i=0;$i<count($masp);$i++){
-			for($j=0;$j<count($thoigian[$i]);$j++){
-				for($k=0;$k<count($dates);$k++){
-					if($thoigian[$i][$j] != $dates[$k]){
-						array_splice($thoigian[$i], $k, 0, 0);
-						array_splice($data[$i], $k, 0, 0);
-					};
+		//sửa data cho đúng ngày, ngày nào không có thì data là 0 
+		for($i=0;$i<count($masp);$i++){
+			for($j=0;$j<count($dates);$j++){
+				if(!in_array($dates[$j],$thoigian[$i])){
+					array_splice($thoigian[$i],$j,0,0);
+					array_splice($data[$i],$j,0,0);
 				};
 			};
-		};*/
-		for($i=0;$i<count($masp);$i++){
-		echo "<pre>";
-		print_r($thoigian[$i]);
-		echo "</pre>";};
+		}; 
 		
-		for($i=0;$i<count($masp);$i++){
 		echo "<pre>";
-		print_r($data[$i]);
-		echo "</pre>";};
+		print_r($dates);
+		echo "</pre>";
+		
+		echo "<pre>";
+		print_r($thoigian);
+		echo "</pre>";
+		
+		
+		echo "<pre>";
+		print_r($data);
+		echo "</pre>";
 		
 		?>
 		<script type="text/javascript">//biểu đồ
 		$(function () {
 		    $('#container').highcharts({
 		        title: {
-		            text: <?php echo "'"."$ten"."'"; ?>,
+		            text: <?php echo "'"."$tenbieudo"."'"; ?>,
 		            x: -20 //center
 		        },
 		        subtitle: {
@@ -136,7 +137,7 @@
 		            }]
 		        },
 		        tooltip: {
-		            valueSuffix: '°C'
+		            valueSuffix: ''
 		        },
 		        legend: {
 		            layout: 'vertical',
@@ -144,19 +145,12 @@
 		            verticalAlign: 'middle',
 		            borderWidth: 0
 		        },
-		        series: [{
-		            name: 'Tokyo',
-		            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-		        }, {
-		            name: 'New York',
-		            data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
-		        }, {
-		            name: 'Berlin',
-		            data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
-		        }, {
-		            name: 'London',
-		            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-		        }]
+		        series: [<?php 
+		        	for($i=0;$i<count($masp);$i++){
+		        		$data[$i] = implode(",",$data[$i]);
+		        		echo "{name: "."'".$name[$i]."'".","."data: "."[".$data[$i]."]"."},";
+		        	};
+		        ?>]
 		    });
 		});
 		</script>
